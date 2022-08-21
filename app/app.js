dd8d8b4d3aa866ea53121f9667c9e70b807d1ab8
Takarkuna <takarkuna@gmail.com>
2022-08-20 21:13:21 -0500

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,12 @@
+import {
+  Application,
+  AndroidApplication,
+  isAndroid,
+  Device,
+  TabView,
+  TabViewItem,
+  SelectedIndexChangedEventData,
+} from '@nativescript/core';
+('tns-core-modules/ui/tab-view');
+
+Application.run({ moduleName: 'app-root' });
